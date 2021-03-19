4ea0ff8056f5935506ddef0d805e2b0d1eab7019
Baptiste Lemonnier <>
2021-03-19 15:31:16 +0100

    ajout style index + ajout nb commande + ajout promotion en cours

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -2,9 +2,13 @@
 namespace controllers;
 
  use Ajax\php\ubiquity\JsUtils;
+ use models\Order;
  use Ubiquity\attributes\items\router\Route;
  use Ubiquity\controllers\auth\AuthController;
  use Ubiquity\controllers\auth\WithAuthTrait;
+ use models\Product;
+ use Ubiquity\orm\DAO;
+ use Ubiquity\utils\http\USession;
 
  /**
   * Controller MainController
@@ -17,7 +21,10 @@ class MainController extends ControllerBase{
 
     #[Route('_default',name: 'home')]
     public function index(){
-        $this->jquery->renderView("MainController/index.html");
+        //$this->jquery->renderView("MainController/index.html"); pourquoi ça marche sans ??
+        $productPromo = DAO::getAll(Product::class, 'promotion< ?', false, [0]);
+        $nbCommand = DAO::count(Order::class, 'idUser=?', [USession::get("idUser")]);
+        $this->loadDefaultView(['productPromo'=>$productPromo, 'nbCommand'=>$nbCommand]);
     }
 
     protected function getAuthController(): AuthController
